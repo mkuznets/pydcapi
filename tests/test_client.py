61e08e3ebbffa313b30d04eb7678f97220e7f235
Max Kuznetsov <maks.kuznetsov@gmail.com>
2024-03-04 22:30:33 +0000

    Add accidentally omitted test

--- tests/test_client.py
+++ tests/test_client.py
@@ -13,3 +13,9 @@ def test_folders_list_root(client_from_env: pydcapi.Client):
     root_uri = str(system_folders.roots.document_cloud.root_uri)
 
     client_from_env.folders.list(folder_uri=root_uri, order_by="name", sort_order="ascending")
+
+
+def test_users_get_user(client_from_env: pydcapi.Client):
+    user = client_from_env.users.get_user(fields='identity')
+    assert user.identity.category == 'adobeid'
+    assert user.identity.email
